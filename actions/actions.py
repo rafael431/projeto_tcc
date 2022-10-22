a7eda35ef5b348ab21a79ed3705c9b3e5322a1d7
rafael431 <rtc431@gmail.com>
2022-10-22 14:03:10 -0300

    TCC: First Commit

---
+++ actions/actions.py
@@ -0,0 +1,50 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+#This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        #Getting the latest user message.
+        for event in tracker.events:
+            if event.get("event") == "user":
+                latestMessage = event.get("text")
+
+        dispatcher.utter_message(text=latestMessage)
+        return [SlotSet("classification_result", "true")]
+
+class FakeNewsClassifier(Action):
+
+    def name(self) -> Text:
+        return "action_classifier"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        #Getting the latest user message.
+        for event in tracker.events:
+            if event.get("event") == "user":
+                latestMessage = event.get("text")
+
+        #Check if dispatcher is necessary.
+        dispatcher.utter_message(text=latestMessage)
+
+        #Insert classifier logic.
+        return [SlotSet("classification_result", "true")]
